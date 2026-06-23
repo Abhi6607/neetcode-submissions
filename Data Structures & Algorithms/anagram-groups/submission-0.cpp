41eed6c49e11931bffb65ccdac4f58fc723d914a
Abhi6607 <abhinavmallampati@gmail.com>
2026-06-23 13:07:44 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<array<int, 26>, vector<string>> mp;
+
+        for (string& s : strs) {
+            array<int, 26> freq = {};
+            for (char c : s) freq[c - 'a']++;
+            mp[freq].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto& [key, group] : mp) ans.push_back(group);
+
+        return ans;
+    }
+};
